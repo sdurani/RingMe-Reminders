56f9331e95def730b118415f23b2cf584aef5368
[sama durani] <duranisama@gmail.com>
2024-10-11 21:22:29 -0400

    created swift project with initial files

---
+++ capstone/capstone/capstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  capstoneApp.swift
+//  capstone
+//
+//  Created by Sama Durani on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct capstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

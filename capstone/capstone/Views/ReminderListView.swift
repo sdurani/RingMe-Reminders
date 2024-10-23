bd0776db55de9eb8a2c3cb2ea7dad8511d49d075
[sama durani] <duranisama@gmail.com>
2024-10-23 16:39:47 -0400

    fetch reminders by list functionality complete

---
+++ capstone/capstone/Views/ReminderListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderListView.swift
+//  capstone
+//
+//  Created by Sama Durani on 10/23/24.
+//
+
+import SwiftUI
+
+struct ReminderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderListView()
+}

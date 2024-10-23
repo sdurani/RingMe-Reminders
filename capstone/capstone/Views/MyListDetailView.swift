4016d90f0bdfbabfdbc93438b116942207d25bf7
[sama durani] <duranisama@gmail.com>
2024-10-23 16:07:11 -0400

    updated reminder service and detail list view

---
+++ capstone/capstone/Views/MyListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListDetailView.swift
+//  capstone
+//
+//  Created by Sama Durani on 10/23/24.
+//
+
+import SwiftUI
+
+struct MyListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListDetailView()
+}

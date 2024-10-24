d27905a3aa04468d3d2c896cc98db2383af20793
[sama durani] <duranisama@gmail.com>
2024-10-24 12:22:22 -0400

    moree renaming

--- capstone/RingMeRemindersApp/RingMeRemindersAppApp.swift
+++ capstone/RingMeRemindersApp/RingMeRemindersAppApp.swift
@@ -1,30 +1,17 @@
 //
-//  capstoneApp.swift
-//  capstone
+//  RingMeRemindersAppApp.swift
+//  RingMeRemindersApp
 //
-//  Created by Sama Durani on 10/11/24.
+//  Created by Sama Durani on 10/24/24.
 //
 
 import SwiftUI
-import UserNotifications
 
 @main
 struct RingMeRemindersAppApp: App {
-    
-    init() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if granted {
-                // notification granted
-            } else {
-                // display message to user
-            }
-        }
-    }
-    
     var body: some Scene {
         WindowGroup {
-            HomeView()
-                .environment(\.managedObjectContext, CoreDataProvider.shared.persistentContainer.viewContext)
+            ContentView()
         }
     }
 }
